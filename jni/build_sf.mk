1eb41029a6fb4db5f3ab4a84e1e0ac0c6351d0c1
Gerhard Kalab <gkalab@users.noreply.github.com>
2025-08-15 17:42:19 +0200

    Support 16KB page sizes

--- jni/build_sf.mk
+++ jni/build_sf.mk
@@ -1,5 +1,7 @@
 LOCAL_SRC_FILES := $(SF_SRC_FILES)
 LOCAL_CFLAGS    := -std=c++17 -O3 -fno-exceptions -DNNUE_EMBEDDING_OFF -DUSE_PTHREADS \
                    -fPIE $(MY_ARCH_DEF) -s -flto=thin
-LOCAL_LDFLAGS	+= -fPIE -s -flto=thin
+LOCAL_LDFLAGS	+= -fPIE -s -flto=thin -lm
+LOCAL_LDFLAGS   += "-Wl,-z,max-page-size=16384"
+LOCAL_LDFLAGS   += "-Wl,-z,common-page-size=16384"
 include $(BUILD_EXECUTABLE)
